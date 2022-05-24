aac3fde20e0c9e55c304a86be58cc549a2ea6118
Ivan Mauricio Diaz <idiaz@conexia.com>
2022-05-24 00:26:56 -0500

    second one piece character

--- src/app/shared/constants/category-image.enum.ts
+++ src/app/shared/constants/category-image.enum.ts
@@ -5,9 +5,10 @@ export class ECategoryImage {
   name: string;
   folder: string;
 
-  public static GEIC001 = new ECategoryImage(1, 'OPIC001', 'location-gallery', 'assets/img/one-piece/location');
-  public static GEIC002 = new ECategoryImage(2, 'OPIC002', 'character-list', 'assets/img/one-piece/character-list');
-  public static GEIC003 = new ECategoryImage(2, 'OPIC003', 'character-list', 'assets/img/one-piece/character-detail/luffy');
+  public static OPIC001 = new ECategoryImage(1, 'OPCI001', 'location-gallery', 'assets/img/one-piece/location');
+  public static OPIC002 = new ECategoryImage(2, 'OPCI002', 'character-list', 'assets/img/one-piece/character-list');
+  public static OPIC003 = new ECategoryImage(3, 'OPCI003', 'character-list', 'assets/img/one-piece/character-detail/luffy');
+  public static OPIC004 = new ECategoryImage(4, 'OPCI004', 'character-list', 'assets/img/one-piece/character-detail/zoro');
 
 
   constructor(id: number, code: string, name: string, folder: string) {
