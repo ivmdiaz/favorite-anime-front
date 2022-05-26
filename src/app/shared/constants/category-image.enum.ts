9b7a67480199eea5e05bc9fe4e9d30958bf4a28a
Ivan Mauricio Diaz <idiaz@conexia.com>
2022-05-25 23:13:03 -0500

    add three characters

--- src/app/shared/constants/category-image.enum.ts
+++ src/app/shared/constants/category-image.enum.ts
@@ -9,6 +9,9 @@ export class ECategoryImage {
   public static OPIC002 = new ECategoryImage(2, 'OPCI002', 'character-list', 'assets/img/one-piece/character-list');
   public static OPIC003 = new ECategoryImage(3, 'OPCI003', 'character-list', 'assets/img/one-piece/character-detail/luffy');
   public static OPIC004 = new ECategoryImage(4, 'OPCI004', 'character-list', 'assets/img/one-piece/character-detail/zoro');
+  public static OPIC005 = new ECategoryImage(5, 'OPCI005', 'character-list', 'assets/img/one-piece/character-detail/sanji');
+  public static OPIC006 = new ECategoryImage(6, 'OPCI006', 'character-list', 'assets/img/one-piece/character-detail/nami');
+  public static OPIC007 = new ECategoryImage(7, 'OPCI007', 'character-list', 'assets/img/one-piece/character-detail/chopper');
 
 
   constructor(id: number, code: string, name: string, folder: string) {
